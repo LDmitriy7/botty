58ac11f59ac7e05c5dce5f7ed1e2628dae09a892
levch <levchenko.d.a1998@gmail.com>
2023-03-24 12:15:59 +0600

    fix QueryHandler build

--- botty/errors.py
+++ botty/errors.py
@@ -5,3 +5,11 @@ class FieldError(AttributeError):
 
     def __str__(self) -> str:
         return f"No `{self.field}` for `{self.obj}`"
+
+
+class CallbackDataError(ValueError):
+    def __init__(self, callback_data: object) -> None:
+        self.callback_data = callback_data
+
+    def __str__(self) -> str:
+        return f"Invalid callback_data: {self.callback_data}"
--- botty/handlers/classes/query.py
+++ botty/handlers/classes/query.py
@@ -1,11 +1,29 @@
 from abc import ABC
 
+from telegram import ext
+
+from botty.errors import CallbackDataError
+from botty.handlers.types import PTBHandler
+from botty.helpers import listify
 from botty.types import Query
 
 from .update import UpdateHandler
 
 
 class QueryHandler(UpdateHandler, ABC):
+    on_button: str | list[str]
+
+    @classmethod
+    def build(cls) -> PTBHandler:
+        return ext.CallbackQueryHandler(cls._handle, cls._filter)
+
+    @classmethod
+    def _filter(cls, callback_data: object) -> bool:
+        if not isinstance(callback_data, str):
+            raise CallbackDataError(callback_data)
+        cls._validate_class_field("on_button")
+        return callback_data in listify(cls.on_button)
+
     async def answer(self, text: str, *, show_alert: bool = False) -> bool:
         return await self.query.answer(text, show_alert=show_alert)
 
